bb7e7cf95d9bd8ad3f3d116ba3d6d7046abab388
Jack Davidson <jackmaverickdavidson@gmail.com>
2021-04-20 17:58:00 -0500

    add

--- blocks.h
+++ blocks.h
@@ -1,14 +1,19 @@
-//#define LT0
 //#define DT0
+#define LT0
+
+#ifdef LT0
+#define ALSA_VOLUME_COMMAND "amixer get Master | awk -F'[][]' 'END{ print $2 }'"
+#endif
+
+#ifdef DT0
+#define ALSA_VOLUME_COMMAND "amixer get PCM | awk -F'[][]' 'END{ print $2 }'"
+#endif
 
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/* Icon */ /*Command*/						/*Update Interval*/	/*Update Signal*/
-#ifdef DT0
-	{" vol: ", "amixer -c 2 get PCM | awk -F'[][]' 'END{ print $2 }'",	1,		0},
-#endif
+	{" vol: ", ALSA_VOLUME_COMMAND,						1,		0},
 #ifdef LT0
-	{" vol: ", "amixer get Master | awk -F'[][]' 'END{ print $2 }'",	1,		0},
 	{" bat: ", "echo $cat /sys/class/power_cupply/BAT0/capacity)%, $(cat/sys/class/power_supply/BAT1/capacity)%", 30, 0},
 #endif
 	{"mem: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
