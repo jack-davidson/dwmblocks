a9ad708ed6f2067f7355bfe6a4016dd0a13a2ad8
Jack Davidson <jackmaverickdavidson@gmail.com>
2021-04-20 16:04:00 -0500

    default

--- blocks.h
+++ blocks.h
@@ -1,5 +1,5 @@
 //#define LT0
-#define DT0
+//#define DT0
 
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
