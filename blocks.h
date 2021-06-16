11e492bc1e39ac2aca0fa4f158d86263c542ac8d
Jack Davidson <jackmaverickdavidson@gmail.com>
2021-06-16 09:37:47 -0500

    updated blocks in order to clean things up and added unicode emojis in order to make it look fancy

--- blocks.h
+++ blocks.h
@@ -1,25 +1,25 @@
 /* #define DT0 */
-/* #define LT0 */
+#define LT0
 
 #ifdef LT0
-#define ALSA_VOLUME_COMMAND "amixer get Master | awk -F'[][]' 'END{ print $2 }'"
+#define ALSA_DEVICE "Master"
 #endif
 
 #ifdef DT0
-#define ALSA_VOLUME_COMMAND "amixer get PCM | awk -F'[][]' 'END{ print $2 }'"
+#define ALSA_DEVICE "PCM"
 #endif
 
 /* Modify this file to change what commands output to your statusbar, and recompile using the make command. */
 static const Block blocks[] = {
 	/* Icon */ /*Command*/						/*Update Interval*/	/*Update Signal*/
-	{" vol: ", ALSA_VOLUME_COMMAND,						1,		0},
+	{" 墳: ", "amixer get " ALSA_DEVICE " | awk -F'[][]' 'END{ print $2 }'",						1,		0},
 #ifdef LT0
-	{" bat: ", "echo $(cat /sys/class/power_supply/BAT0/capacity)%, $(cat /sys/class/power_supply/BAT1/capacity)%", 30, 0},
+	{" : ", "echo $(cat /sys/class/power_supply/BAT0/capacity)%, $(cat /sys/class/power_supply/BAT1/capacity)%", 30, 0},
 #endif
-	{"mem: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{": ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 	{"", "date '+%b %d (%a) %I:%M %p '",					5,		0}
 };
 
 /* sets delimeter between status commands. NULL character ('\0') means no delimeter. */
-static char delim[] = " | ";
+static char delim[] = " / ";
 static unsigned int delimLen = 5;
