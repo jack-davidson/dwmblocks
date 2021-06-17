401298544141b5c9a1b0ac43ebbf7ec23e0d046f
Jack Davidson <jackmaverickdavidson@gmail.com>
2021-06-17 17:32:40 -0500

    fixed script

--- scripts/mpd.sh
+++ scripts/mpd.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 title=$(mpc -f "%title%" | sed 1q)
 artist=$(mpc -f "%artist%" | sed 1q)
-time=$(mpc | sed '2!d' | cut -d' ' -f6)
+time=$(mpc | sed "2!d" | tr -s ' ' | cut -d' ' -f3)
 echo "ﱘ  ${artist} - ${title} (${time})"
