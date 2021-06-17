ff95dd998be2232f34d39ee0d5e131d6e2b0a62a
Jack Davidson <jackmaverickdavidson@gmail.com>
2021-06-17 17:37:39 -0500

    removed icon construct

--- blocks.h
+++ blocks.h
@@ -8,14 +8,14 @@
 
 /* Modify this file to change what commands output to your statusbar, and recompile using the make command. */
 static const Block blocks[] = {
-	/* Icon */ 	/*Command*/							/*Update Interval*/	/*Update Signal*/
-	{" ",		SCRIPT("vol.sh " ALSA_DEVICE),					1,			0},
-	{"",		SCRIPT("mpd.sh"),						1,			0},
+	/*Command*/							/*Update Interval*/	/*Update Signal*/
+	{SCRIPT("vol.sh " ALSA_DEVICE),					1,			0},
+	{SCRIPT("mpd.sh"),						1,			0},
 #ifdef lt0
-	{"",		SCRIPT("bat.sh"),						30,			0},
+	{SCRIPT("bat.sh"),						30,			0},
 #endif
-	{"  ",	"free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,			0},
-	{"",		SCRIPT("date.sh"),						5,			0}
+	{"free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,			0},
+	{SCRIPT("date.sh"),						5,			0}
 };
 
 /* sets delimeter between status commands. NULL character ('\0') means no delimeter. */
