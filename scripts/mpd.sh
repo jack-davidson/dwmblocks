70ee8114768fdd7d79c679c40aa648c98b9f4243
Jack Davidson <jackmaverickdavidson@gmail.com>
2021-06-17 21:12:19 -0500

    update

--- scripts/mpd.sh
+++ scripts/mpd.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-title=$(mpc -f "%title%" | sed 1q)
-artist=$(mpc -f "%artist%" | sed 1q)
-time=$(mpc | sed "2!d" | tr -s ' ' | cut -d' ' -f3)
-echo "ﱘ  ${artist} - ${title} (${time})"
+
+current=$(mpc current)
+[ -z "${current}" ] && current="mpd idle"
+time=" ($(mpc | sed "2!d" | tr -s ' ' | cut -d' ' -f3))"
+[ "${time}" = "()" ] && time=""
+echo "ﱘ  ${current}${time}"
