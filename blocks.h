5cad60f50936ef55eeba37c0cf425ec0b8043c4e
Jack Davidson <jackmaverickdavidson@gmail.com>
2021-06-20 13:11:16 -0500

    update wl.sh script

--- blocks.h
+++ blocks.h
@@ -13,7 +13,7 @@ static const Block blocks[] = {
 	{SCRIPT("mpd.sh"),		1,			0},
 #ifdef lt0
 	{SCRIPT("bat.sh"),		30,			0},
-	{SCRIPT("wl.sh"),		30,			0},
+	{SCRIPT("wl.sh"),		1,			0},
 #endif
 	{SCRIPT("mem.sh"),		30,			0},
 	{SCRIPT("date.sh"),		5,			0}
--- scripts/wl.sh
+++ scripts/wl.sh
@@ -3,7 +3,11 @@
 interface="$(iwctl station list | grep ".*connected" |
     tr -s ' ' | cut -d' ' -f2)"
 
+
 ssid="$(iwctl station ${interface} show | sed '7!d' |
     tr -s ' ' | sed "s/[ \t]*$//" | cut -d' ' -f4-)"
+
+[ -z "${interface}" ] && echo "泌 n/a" && exit
+[ -z "${ssid}" ] && echo "泌 ${interface}: disconnected"
 
 echo "泌 ${interface}: \"${ssid}\""
