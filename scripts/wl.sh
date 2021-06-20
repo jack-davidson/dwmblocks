230ff8730c0f74a2f7fc1481eac78e7ca20da322
Jack Davidson <jackmaverickdavidson@gmail.com>
2021-06-20 10:57:04 -0500

    updated indentation in wl.sh

--- scripts/wl.sh
+++ scripts/wl.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-interface="$(iwctl station list | grep ".*connected" | tr -s ' ' | cut -d' ' -f2)"
-ssid="$(iwctl station ${interface} show | sed "7!d" | tr -s ' ' | sed "s/[ \t]*$//" | cut -d' ' -f4-)"
+
+interface="$(iwctl station list | grep ".*connected" |
+    tr -s ' ' | cut -d' ' -f2)"
+
+ssid="$(iwctl station ${interface} show | sed "7!d" |
+    tr -s ' ' | sed "s/[ \t]*$//" | cut -d' ' -f4-)"
+
 echo "泌 ${interface}: \"${ssid}\""
