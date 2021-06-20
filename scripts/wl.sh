146c51b8907f582fbc3cc47be547d806a3d1b1b5
Jack Davidson <jackmaverickdavidson@gmail.com>
2021-06-20 10:54:04 -0500

    updated script

--- scripts/wl.sh
+++ scripts/wl.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 interface="$(iwctl station list | grep ".*connected" | tr -s ' ' | cut -d' ' -f2)"
-echo "泌 ${interface}: \"$(iwctl station ${interface} show | sed "7!d" | tr -s ' ' | sed "s/[ \t]*$//" | cut -d' ' -f4-)\""
+ssid="$(iwctl station ${interface} show | sed "7!d" | tr -s ' ' | sed "s/[ \t]*$//" | cut -d' ' -f4-)"
+echo "泌 ${interface}: \"${ssid}\""
